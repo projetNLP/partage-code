ffbbe85181de9c952b45d081ee65e519d27266ef
Junyi <88294680+Vesperer@users.noreply.github.com>
2021-12-21 21:51:32 +0100

    Update model.py

--- model.py
+++ model.py
@@ -1,4 +1,30 @@
 import json
+import os
 
-def read_question():
-    pass
+#lire le fichier json
+def lire_fichier():
+    with open('test_config.json', "r") as f:
+        donnee = json.load(f)
+        liste1 = []
+        liste2 = []
+        image = donnee[num_image]
+        for keys in image:
+            liste2.append(keys)
+        liste1.append(liste2[0])
+        liste1.append(liste2[1])
+        liste2.pop(0)
+        liste2.pop(1)
+    return liste1, liste2
+
+#retourne le nom et l'emplacement des images
+#pour accéder au répertoire d'image avec appjar utiliser: .setImageLocation(location)
+def lire_emplacement():
+    return lire_fichier()[0]
+
+#lire les questions
+def lire_question():
+    return lire_fichier()[1]
+
+#renvoie une liste des noms d'images
+def recupere_image():
+    return os.walk(lire_emplacement()[1]).next()[2]
