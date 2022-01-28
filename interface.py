9da54b123819bd6c186e8eb147ad1fb2b3b40494
sunhaozhepy <sunhaozhe050722a@qq.com>
2022-01-28 11:29:22 +0100

    commit

--- interface.py
+++ interface.py
@@ -5,34 +5,40 @@ import model
 app = gui("interface", "1000x700")
 
 
-def suivant(image):
+def suivant():
     L = [app.getOptionBox(f"{i}") for i in range(5)]
-    print(L)
-    model.enregistre_reponse(image, L)
-    img = ImageTk.PhotoImage(Image.open(
-    model.lire_fichier()[0] + '/' + model.retrouve_image()))
-    # app.addImageData("pic", ImageTk.PhotoImage(Image.open(
-    #     model.lire_fichier()[0] + '/' + model.retrouve_image())), fmt="PhotoImage")
+    image_path = model.retrouve_image()[0]
+    model.enregistre_reponse(image_path, L)
+    app.stop()
 
 
-def interface(image, Q, L):
-    app.setFont(size=20, family="Pumpkin Soup", underline=False, slant="roman")
+def interface(image_resize, Q, L):
+    app.setFont(size=20, family="Verdana", underline=False, slant="roman")
 
     with app.frame("LEFT", row=0, column=0, bg='white', sticky='NEW', stretch='COLUMN'):
         app.addLabel("en-tête", "outil de classification d'images")
-        app.addImageData("pic", image, fmt="PhotoImage")
-        app.setImageSize("pic", 100, 690)
+        app.setLabelBg("en-tête", "DarkCyan")
+        app.setLabelHeight("en-tête", 2)
+        app.addImageData("pic", image_resize, fmt="PhotoImage")
+
     with app.frame("RIGHT", row=0, column=1, bg='white', fg='black'):
         for x in range(len(Q)):
             app.label(Q[x], bg='white')
             app.addLabelOptionBox(str(x), ["- select -"]+L[x])
-        app.addButton("Suivant", suivant(image))
+            app.setLabelBg(Q[x], "bisque")
+
+        app.addButton("suivant", suivant)
+        app.setButtonFg("suivant", "white")
+        app.setButtonBg("suivant", "limegreen")
+        app.setButtonWidth("suivant", 10)
+        app.setButtonHeight("suivant", 1)
+
     app.go()
 
-
-model.init_annotation()
-image = ImageTk.PhotoImage(Image.open(
-    model.lire_fichier()[0] + '/' + model.retrouve_image()))
+model.init_annotation() # n'utiliser cette ligne que pour la première fois d'annotation pour un répertoire d'images
 Q = model.lire_fichier()[1]
 L = model.lire_fichier()[2]
-interface(image, Q, L)
+for img in model.retrouve_image():
+        image = Image.open(model.lire_fichier()[0] + '/' + img)
+        image_resize = ImageTk.PhotoImage(image.resize((600, 600)))
+        interface(image_resize, Q, L)
